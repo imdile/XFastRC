e219c17349ffddf131a22d99be2fb2fee89d2062
imdile <141243906+imdile@users.noreply.github.com>
2026-07-21 15:04:22 +0700

    Add files via upload

---
+++ v25/static/js/ritial.js
@@ -0,0 +1,1 @@
+document.getElementById("uloadermain").classList.remove("uloader-hiddex"),document.getElementById("uloadermain").classList.add("uloader-viewex"),window.onload=function(){document.getElementById("uloadermain").classList.remove("uloader-viewex"),document.getElementById("uloadermain").classList.add("uloader-hiddex")};
